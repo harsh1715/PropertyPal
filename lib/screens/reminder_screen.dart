580192aa622767afbc5f9437e91cbfa3f7f64c72
Harsh Patel <harsh.patel20@ontariotechu.net>
2023-11-06 23:29:42 -0500

    Reminder Screen

--- lib/screens/reminder_screen.dart
+++ lib/screens/reminder_screen.dart
@@ -1,10 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'login_screens.dart';
 
 class ReminderScreen extends StatefulWidget {
-  const ReminderScreen({super.key});
+  const ReminderScreen({Key? key});
 
   @override
   State<ReminderScreen> createState() => _ReminderScreenState();
@@ -12,39 +11,127 @@ class ReminderScreen extends StatefulWidget {
 
 class _ReminderScreenState extends State<ReminderScreen> {
   var isLogoutLoading = false;
-  logOut() async {
-    setState(() {
-      isLogoutLoading = true;
-    });
-    await FirebaseAuth.instance.signOut();
-    Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => Login()),
-    );
 
-    setState(() {
-      isLogoutLoading = false;
-    });
+
+
+  // Function to handle navigation to the next page
+  void navigateToAnotherPage() {
+    // Replace with your navigation logic
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => AnotherPage()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // backgroundColor: Colors.blue.shade900,
-        title: Text("Reminders",style: TextStyle(color: Colors.white),),
+        title: Text("Reminders", style: TextStyle(color: Colors.black)),
         centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: (){
-              logOut();
-            },
-            icon: isLogoutLoading
-                ? CircularProgressIndicator()
-                : Icon(Icons.exit_to_app,),
-          )
-        ],
       ),
-      body: Text("This is Reminder Screen"),
+      body: DefaultTabController(
+        length: 2,
+        child: Column(
+          children: [
+            const TabBar(
+              labelColor: Colors.black, // Tab label color
+              tabs: [
+                Tab(
+                  child: Text(
+                    "by Property",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+                Tab(
+                  child: Text(
+                    "by Status",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: [
+                  ListView(
+                    // padding: const EdgeInsets.only(top: 16),
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 40,
+                              decoration: BoxDecoration(
+                                color: Colors.blue.shade200,
+                              ),
+                              child: const Align(
+                                alignment: Alignment.centerLeft, // Left-align the text
+                                child: Padding(
+                                  padding: EdgeInsets.only(left: 16), // Adjust the left padding
+                                  child: Text(
+                                    "544 Winchester - House",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      // Add more widgets to the list if needed
+
+                      // Add a styled ListTile for navigation to another page
+                      ListTile(
+                        contentPadding: EdgeInsets.all(16), // Padding for ListTile content
+                        title: Text("Outstanding/Late Rent Payment"),
+                        trailing: Icon(Icons.navigate_next), // Add a ">" icon to the right
+                        tileColor: Colors.grey, // Background color for the ListTile
+                        subtitle: const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.warning,
+                                  color: Colors.red,
+                                  size: 20,
+                                ),
+                                Text(
+                                  " \$1,600.00, 5 Days Late, James Richard",
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        onTap: navigateToAnotherPage,
+                      )
+                    ],
+                  ),
+                  const Center(
+                    child: Text("Content for Status Tab"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Define the AnotherPage widget for navigation
+class AnotherPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("James Richard"),
+      ),
+      body: Center(
+        child: Text("STuff"),
+      ),
     );
   }
 }
