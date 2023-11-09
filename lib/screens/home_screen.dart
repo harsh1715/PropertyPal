fc255ea428c0baa5e7688e23c8c48d9b0b793f93
Georges Anthony Gutierrez Ricard <georges.gutierrezricard@ontariotechu.net>
2023-11-08 23:50:27 -0500

    Welcome screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:propertypal/screens/welcome_screen.dart';
 
 import '../widgets/add_property_form.dart';
 import 'login_screens.dart';
@@ -35,12 +36,6 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddPropertyForm()));
-        } ,
-        child: Icon(Icons.add),
-      ),
       appBar: AppBar(
         title: Text(
           "All Properties",
@@ -50,12 +45,12 @@ class _HomeScreenState extends State<HomeScreen> {
         actions: [
           IconButton(
             onPressed: () {
-              logOut();
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddPropertyForm()));
             },
             icon: isLogoutLoading
                 ? CircularProgressIndicator()
                 : Icon(
-              Icons.exit_to_app,
+              Icons.add_circle_outline,
             ),
           )
         ],
@@ -64,215 +59,6 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
-// class HouseWidget extends StatelessWidget {
-//   HouseWidget({
-//     super.key,
-//     required this.userId
-//   });
-//
-//   final String? userId;
-//
-//   Widget build(BuildContext context) {
-//     final Stream<DocumentSnapshot> _usersStream = FirebaseFirestore.instance.collection('users').doc(userId).snapshots();
-//     return StreamBuilder<DocumentSnapshot>(
-//       stream: _usersStream,
-//       builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-//         if (snapshot.hasError) {
-//           return Text('Something went wrong');
-//         }
-//         if (!snapshot.hasData || !snapshot.data!.exists){
-//           return const Text("Document does not exist");
-//         }
-//
-//         if (snapshot.connectionState == ConnectionState.waiting){
-//           return Text("Loading");
-//         }
-//         var data = snapshot.data!.data() as Map<String, dynamic>;
-//
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return Text("Loading");
-//         }
-//
-//         return Houses(
-//           data: data, heading: 'Home',
-//         );
-//       },
-//     );
-//   }
-// }
-//
-// class Houses extends StatelessWidget {
-//   const Houses({
-//     super.key, required this.data, required this.heading,
-//   });
-//
-//   final Map data;
-//   final String heading;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: 5, // Change this to the number of containers you want
-//       itemBuilder: (context, index) {
-//         return Container(
-//           width: double.infinity,
-//           height: 210,
-//           color: Colors.blue.shade200,
-//           child: Column(
-//             children: [
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Container(
-//                     alignment: Alignment.topLeft,
-//                     padding: EdgeInsets.only(left: 8, top: 8, bottom: 8),
-//                     child: CircleAvatar(
-//                       radius: 38,
-//                       backgroundColor: Colors.black,
-//                     ),
-//                   ),
-//                   Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Container(
-//                         padding: EdgeInsets.only(top: 8, right: 8),
-//                         child: Row(
-//                           children: [
-//                             Icon(Icons.home_filled, color: Colors.black54),
-//                             SizedBox(width: 8),
-//                             Text("${data['propertyName']}",
-//                                 style: TextStyle(fontSize: 18)),
-//                           ],
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.end,
-//                 children: [
-//                   Container(
-//                     padding: EdgeInsets.only(bottom: 2),
-//                     child: Row(
-//                       mainAxisAlignment: MainAxisAlignment.end,
-//                       children: [
-//                         Text("${data['propertyAddress']}",
-//                             style: TextStyle(fontSize: 12)),
-//                       ],
-//                     ),
-//                   ),
-//                 ],
-//               ),
-//               Stack(
-//                 children: [
-//                   Row(
-//                     children: [
-//                       Padding(
-//                         padding: EdgeInsets.only(left: 12, top: 35),
-//                         child: Transform.rotate(
-//                           angle: -90 * 3.1415926535 / 180,
-//                           child: Container(
-//                             alignment: Alignment.bottomLeft,
-//                             child: Text(
-//                               heading,
-//                               style: TextStyle(
-//                                   fontSize: 12,
-//                                   color: Colors.black54,
-//                                   fontWeight: FontWeight.bold),
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.end,
-//                     children: [
-//                       Container(
-//                         width: 356,
-//                         height: 102,
-//                         color: Colors.white,
-//                         alignment: Alignment.topRight,
-//                         padding: EdgeInsets.only(right: 8, top: 8, bottom: 8),
-//                         child: Row(
-//                           children: [
-//                             Expanded(
-//                               child: Column(
-//                                 crossAxisAlignment: CrossAxisAlignment.start,
-//                                 children: [
-//                                   Padding(
-//                                     padding: EdgeInsets.only(left: 8.0),
-//                                     child: Row(
-//                                       children: [
-//                                         Text(
-//                                           "${data['propertyName']}",
-//                                           style: TextStyle(
-//                                               fontWeight: FontWeight.bold),
-//                                         ),
-//                                       ],
-//                                     ),
-//                                   ),
-//                                   Padding(
-//                                     padding: EdgeInsets.only(left: 8.0),
-//                                     child: Row(
-//                                       children: [
-//                                         Icon(Icons.key, color: Colors.grey),
-//                                         Text(
-//                                           "  10/01/2023 - 10/31/2023",
-//                                           style: TextStyle(
-//                                               color: Colors.black26,
-//                                               fontSize: 15),
-//                                         )
-//                                       ],
-//                                     ),
-//                                   ),
-//                                   Padding(
-//                                     padding: EdgeInsets.only(left: 8.0),
-//                                     child: Row(
-//                                       children: [
-//                                         Text("${data['rent']}",
-//                                             style: TextStyle(
-//                                                 color: Colors.red,
-//                                                 fontSize: 15)),
-//                                       ],
-//                                     ),
-//                                   ),
-//                                   Padding(
-//                                     padding: EdgeInsets.only(
-//                                         left: 8.0, top: 7),
-//                                     child: Row(
-//                                       children: [
-//                                         Text('Tenant Balance',
-//                                             style: TextStyle(
-//                                                 color: Colors.black54,
-//                                                 fontSize: 15)),
-//                                       ],
-//                                     ),
-//                                   ),
-//                                 ],
-//                               ),
-//                             ),
-//                             CircleAvatar(
-//                               radius: 35,
-//                               backgroundColor: Colors.black,
-//                             ),
-//                           ],
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
 
 class HouseWidget extends StatelessWidget {
   HouseWidget({
@@ -335,7 +121,7 @@ class Houses extends StatelessWidget {
         }
 
         if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return const Text("No properties found");
+          return WelcomeScreen();
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,112 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/add_property_form.dart';
+import 'login_screens.dart';
+
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({Key? key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.home,
+                  size: 80, // Increase the size of the icon
+                ),
+              ],
+            ),
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                ),
+                children: [
+                  TextSpan(
+                    text: 'Welcome\n\n',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30, // Increase the font size of "Welcome"
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                    'PropertyPal helps you to become an even better landlord. Hopefully this is the beginning of a beautiful friendship.\n\n'
+                  ),
+                  TextSpan(
+                    text:
+                        'Track tenant payments\n',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                    'Log rent payments, share payment receipts with a singletap. Keep track of tenant balance and payment history.\n\n'
+                  ),
+                  TextSpan(
+                    text:
+                        "Tidy your Finances\n",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                    'Track and categorize expenses easily, keep receipt photos. Get instant reports and data for tax time.\n\n'
+                  ),
+                  TextSpan(
+                    text:
+                    "Don't miss important dates\n",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                    'Reminders for late rent payments, expenses due, upcoming lease renewals, and maintenance/safety checks.',
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20), // Add some space between text and the button
+            ElevatedButton(
+              onPressed: () {
+                // Add functionality here for the button
+                // For example, to navigate to a new screen for property creation
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddPropertyForm(),
+                  ),
+                );
+              },
+              child: Text('Create a Property'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
